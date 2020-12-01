85f6af4c57f5e2d06a128fd7e23edb81bbd6187a
Stéphane Severio <severio.stephane@gmail.com>
2020-12-01 12:44:26 +0100

    Add a figurine ok

--- app/controllers/figurines_controller.rb
+++ app/controllers/figurines_controller.rb
@@ -8,4 +8,24 @@ class FigurinesController < ApplicationController
   def show
     @figurine = Figurine.find(params[:id])
   end
+
+  def new
+    @figurine = Figurine.new
+  end
+
+  def create
+    @figurine = Figurine.new(figurine_params)
+    @figurine.user = current_user
+    if @figurine.save
+      redirect_to "/profile"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def figurine_params
+    params.require(:figurine).permit(:name, :description, :year, :branc, :photo)
+  end
 end
