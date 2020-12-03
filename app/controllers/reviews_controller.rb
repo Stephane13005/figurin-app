f81683a148a72c0eb9b9d51f19457318499ec073
Stéphane Severio <severio.stephane@gmail.com>
2020-12-03 22:16:26 +0100

    reviews of figurines ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+	def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.figurine = policy_scope(Figurine).find(params[:figurine_id])
+    if @review.save
+    redirect_to figurine_path(@review.figurine)
+else
+	render 'figurines/show'
+  end
+end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :figurines, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+      resources :reviews, only: [ :new, :create ]
   	  	member do
   		get 'media'
   	end
