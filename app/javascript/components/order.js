35d33606f1099a1984905cce14ac81081bee805e
Stéphane Severio <severio.stephane@gmail.com>
2020-12-07 16:31:12 +0100

    link ok

--- app/javascript/components/order.js
+++ app/javascript/components/order.js
@@ -1,9 +1,7 @@
 // const paymentButton = document.getElementById('pay');
-        
-
-//   paymentButton.addEventListener('click', () => {
-//           const stripe = Stripe('<%= ENV['STRIPE_PUBLISHABLE_KEY'] %>');
-//           stripe.redirectToCheckout({
-//             sessionId: '<%= @order.checkout_session_id %>'
-//           });
-//         });
+// paymentButton.addEventListener('click', () => {
+// const stripe = Stripe('<%= ENV['STRIPE_PUBLISHABLE_KEY'] %>');
+// stripe.redirectToCheckout({
+// sessionId: '<%= @order.checkout_session_id %>'
+// });
+// });
