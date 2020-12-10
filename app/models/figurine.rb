3d8b1487157617d20d5ffe7c9524ff3370448a47
Stéphane Severio <severio.stephane@gmail.com>
2020-12-10 09:29:18 +0100

    validation figurine ok

--- app/models/figurine.rb
+++ app/models/figurine.rb
@@ -4,10 +4,11 @@ class Figurine < ApplicationRecord
   belongs_to :user
   has_one_attached :photo, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  has_many :favourites
+  has_many :favourites, dependent: :destroy
   has_many :favorited_by, through: :favourites, source: :user
   monetize :price_cents
   has_many :likes, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   include PgSearch::Model
   pg_search_scope :search_by,
